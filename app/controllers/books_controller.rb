39828af32ff80aa8d13a03aa40fe58c6e3be261b
EC2 Default User <ec2-user@ip-172-31-0-162.ap-northeast-1.compute.internal>
2023-08-31 07:07:21 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+    @book.user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       flash[:notice] = "You have created book successfully."
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      flash.now[:notice]="can't be blank"
+      render :index
+    end
+  end
+
+  def edit
+     book = Book.find(params[:id])
+  unless book.user.id == current_user.id
+         redirect_to books_path
+  end
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      flash.now[:notice]="error"
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "/home/about", to: 'homes#about', as: 'about'
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :users, only: [:index,:show,:edit,:update]
+  
+end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
