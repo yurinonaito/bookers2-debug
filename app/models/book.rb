f7c1e8e3586bbc0da7d53a781794bc62ebf2c229
EC2 Default User <ec2-user@ip-172-31-0-162.ap-northeast-1.compute.internal>
2023-09-03 01:54:12 +0000

    kadai5

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,4 +11,19 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("name LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+  
+  
 end
