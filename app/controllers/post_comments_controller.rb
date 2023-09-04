13519c06394897bc50e4686731e0da325dcbcdee
EC2 Default User <ec2-user@ip-172-31-0-162.ap-northeast-1.compute.internal>
2023-09-04 01:59:20 +0000

    kadai6finish

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class PostCommentsController < ApplicationController
   
   def create
-   book = Book.find(params[:book_id])
+   @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
   
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import "bootstrap";
 
 import "../stylesheets/application";
 
+require("jquery")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
